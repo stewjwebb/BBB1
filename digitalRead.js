4e775ff7140073604f6bd8d722219783af0eb4ad
Stew Webb <stewjwebb@gmail.com>
2014-10-19 05:47:05 +0000

    latest

--- digitalRead.js
+++ digitalRead.js
@@ -1,6 +1,6 @@
 var b = require('bonescript');
-b.pinMode('P8_19', b.INPUT);
-b.digitalRead('P8_19', printStatus);
+b.pinMode('P8_11', b.INPUT);
+b.digitalRead('P8_11', printStatus);
 function printStatus(x) {
     console.log('x.value = ' + x.value);
     console.log('x.err = ' + x.err);
---
+++ digitalWrite2.js
@@ -0,0 +1,3 @@
+var b = require('bonescript');
+b.pinMode('P8_11', b.OUTPUT);
+b.digitalWrite('P8_11', b.LOW);
